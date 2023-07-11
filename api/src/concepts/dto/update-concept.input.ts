a7389b3a74f29a81d6eff402be41d304856a324c
Danny Gelman <dannygelman1@gmail.com>
2023-07-10 21:49:37 -0400

    update

--- api/src/concepts/dto/update-concept.input.ts
+++ api/src/concepts/dto/update-concept.input.ts
@@ -1,8 +1,17 @@
 import { CreateConceptInput } from './create-concept.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
+import { InputType, Field, PartialType } from '@nestjs/graphql';
 
 @InputType()
 export class UpdateConceptInput extends PartialType(CreateConceptInput) {
-  @Field(() => Int)
-  id: number;
+  @Field({ description: 'ID of the concept art image', nullable: true })
+  image_id?: string;
+
+  @Field({ description: 'Title of the concept art', nullable: true })
+  title?: string;
+
+  @Field({ description: 'Artist who made the concept art', nullable: true })
+  artist?: string;
+
+  @Field({ description: 'Url to the concept art', nullable: true })
+  url?: string;
 }
