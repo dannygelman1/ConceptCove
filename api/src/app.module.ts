7d26f1f5caa7e98ed6546b196e1427c87c20dc92
Danny Gelman <dannygelman1@gmail.com>
2023-07-10 22:08:39 -0400

    changes to app module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,9 +2,30 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConceptsModule } from './concepts/concepts.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [ConceptsModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      playground: true,
+      cache: 'bounded',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'dpg-cilgav15rnuvtgsgm680-a',
+      port: 5432,
+      username: 'conceptcove_user',
+      password: 'bIqBW9ZyRX2TrlJ0z3fuiABp7TqCCxyq',
+      database: 'conceptcove',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false,
+    }),
+    ConceptsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
