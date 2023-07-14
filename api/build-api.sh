6dd27cf1e01e49f2faf375fdd04060ccd538aeab
Danny Gelman <dannygelman1@gmail.com>
2023-07-14 00:00:10 -0400

    debug

--- api/build-api.sh
+++ api/build-api.sh
@@ -3,9 +3,13 @@
 set -eux
 
 pushd .
+echo "Current directory: $(pwd)"
 
+echo "Running 'npm install'..."
 npm install
+
+echo "Running 'npm run build'..."
 npm run build
 
 popd
-
+echo "Back to directory: $(pwd)"
