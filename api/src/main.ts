9a628ed3141135cd26c31f55b9ef7525a2a7f172
Danny Gelman <dannygelman1@gmail.com>
2023-07-13 22:36:28 -0400

    update main

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,5 +4,17 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  const allowedOrigins: (string | RegExp)[] = [];
+
+  allowedOrigins.push('https://localhost:3000/');
+
+  app.enableCors({
+    origin: allowedOrigins,
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Accept', 'Content-Type', 'Authorization'],
+    credentials: true,
+    maxAge: 3600,
+  });
 }
 bootstrap();
