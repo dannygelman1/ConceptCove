d143b725f4284825024953f6a89f089acf4a46e6
Danny Gelman <dannygelman1@gmail.com>
2023-07-15 16:40:49 -0400

    logs

--- api/src/concepts/concepts.resolver.ts
+++ api/src/concepts/concepts.resolver.ts
@@ -11,16 +11,19 @@ export class ConceptsResolver {
   createConcept(
     @Args('createConceptInput') createConceptInput: CreateConceptInput,
   ): Promise<Concept> {
+    console.log('create', createConceptInput);
     return this.conceptsService.create(createConceptInput);
   }
 
   @Query(() => Concept, { name: 'concept' })
   concept(@Args('id', { type: () => String }) id: string): Promise<Concept> {
+    console.log('id in query', id);
     return this.conceptsService.findOne(id);
   }
 
   @Query(() => [Concept], { name: 'allConcepts' })
   allConcepts(): Promise<Concept[]> {
+    console.log('query all');
     return this.conceptsService.findAll();
   }
 }
