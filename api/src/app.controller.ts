f0383a24f551737e2050c280b61a58c25094e054
Danny Gelman <dannygelman1@gmail.com>
2023-07-15 17:33:57 -0400

    adding options ok

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Options } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Options('*')
+  handleOptions() {
+    // Respond to OPTIONS requests with the appropriate CORS headers
+    return 'OK';
   }
 }
