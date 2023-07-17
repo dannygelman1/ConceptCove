682c4b774f997e50c3ffaee8dafb8286c5c31109
Danny Gelman <dannygelman1@gmail.com>
2023-07-17 00:02:05 -0400

    cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const allowedOrigins: (string | RegExp)[] = [];
   allowedOrigins.push('https://localhost:3000');
   app.enableCors({
