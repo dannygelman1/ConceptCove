15de7a248b83b89d221d97d2e2e3f44a6d1377a3
Danny Gelman <dannygelman1@gmail.com>
2023-07-19 01:07:37 -0400

    attempt

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,20 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // const app = await NestFactory.create(AppModule);
+  // await app.listen(3000);
+
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(3000);
 
-  const allowedOrigins: (string | RegExp)[] = [];
+  // const allowedOrigins: (string | RegExp)[] = [];
 
-  allowedOrigins.push('https://localhost:3000');
-  allowedOrigins.push('https://concept-cove.vercel.app');
+  // allowedOrigins.push('https://localhost:3000');
+  // allowedOrigins.push('https://concept-cove.vercel.app');
 
-  app.enableCors({
-    origin: allowedOrigins,
-    methods: ['GET', 'POST', 'OPTIONS'],
-    allowedHeaders: ['Accept', 'Content-Type', 'Authorization'],
-    credentials: true,
-    maxAge: 3600,
-  });
+  // app.enableCors({
+  //   origin: allowedOrigins,
+  //   methods: ['GET', 'POST', 'OPTIONS'],
+  //   allowedHeaders: ['Accept', 'Content-Type', 'Authorization'],
+  //   credentials: true,
+  //   maxAge: 3600,
+  // });
 }
 bootstrap();
